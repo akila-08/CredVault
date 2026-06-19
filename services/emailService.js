63fe0a4e3645eda299ced332fbcf93518b41af08
Aarthi2005 <aarthinagaraj2005@gmail.com>
2026-06-19 20:53:00 +0530

    Completed student verification and ownership workflow

--- services/emailService.js
+++ services/emailService.js
@@ -2,7 +2,7 @@ import SibApiV3Sdk from "sib-api-v3-sdk";
 
 function getEmailClient() {
   const client = SibApiV3Sdk.ApiClient.instance;
-  client.authentications["api-key"].apiKey = process.env.BREVO_API_KEY;
+  console.log("BREVO_API_KEY =", process.env.BREVO_API_KEY);
   return new SibApiV3Sdk.TransactionalEmailsApi();
 }
 
